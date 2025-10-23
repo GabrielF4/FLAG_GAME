528a915810ae6f51adbd3362dfa1131c03529f4e
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-10-23 14:54:06 +0200

    Imported flags as svg files, integrated country codes, country names and flags to be accessable in the code and added feature to generate countries and flags randomly

--- flag_game/src/App.jsx
+++ flag_game/src/App.jsx
@@ -1,18 +1,49 @@
 import { useState } from "react";
-import startFlag from "./assets/flags/se.svg";
 import "./App.css";
+import countries from "./scripts/countries.json" assert { type: "json" };
+
+const NR_OF_COUNTRIES = Object.keys(countries).length;
+const START_FLAG = "SE";
+
+function getRandomCountry() {
+    const randNum = Math.floor(Math.random() * NR_OF_COUNTRIES);
+    return Object.keys(countries)[randNum];
+}
+
+function getFlagUrl(flagCode) {
+    return new URL(
+        `./assets/flags/${flagCode.toLowerCase()}.svg`,
+        import.meta.url
+    ).href;
+}
 
 function App() {
-    //const [count, setCount] = useState(0);
+    const [country, setCountry] = useState(START_FLAG);
 
     return (
         <>
             <div>
-                <img src={startFlag} className="logo react" alt="Start flag" />
+                <img
+                    src={getFlagUrl(country)}
+                    className="logo react"
+                    alt="Start flag"
+                />
             </div>
             <h1>Flag Game</h1>
             <div className="card">
-                <button onClick={() => console.log("button clicked")}>
+                <button
+                    onClick={() => {
+                        const nextCountry = getRandomCountry();
+                        setCountry(() => nextCountry);
+                        console.log(
+                            nextCountry +
+                                ", " +
+                                countries[nextCountry] +
+                                ", " +
+                                getFlagUrl(nextCountry)
+                        );
+                    }}
+                >
                     Start Game
                 </button>
                 <p>Press button to start a guess the flag game</p>
